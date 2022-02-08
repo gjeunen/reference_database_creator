7f5f866a95595695f457a9b3927aca42259b9286
hughcross <hughbcross@gmail.com>
2022-02-09 12:35:29 +1300

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='crabs',
+      description='CRABS: Creating Reference databases for Amplicon-Based Sequencing',
+      author='Gert-Jan Jeunen',
+	  author_email='gjeunen@gmail.com',
+	  url='https://github.com/gjeunen/reference_database_creator',
+	  version='0.1.1',
+      packages=['function'],
+      scripts=['crabs']
+)
